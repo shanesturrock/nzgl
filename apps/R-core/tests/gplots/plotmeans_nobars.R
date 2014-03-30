c14e5ffae9337c72182021f10dc7c2fbdbf95d8d
Biomatters NZGL Build User <nzgl@biomatters.com>
2014-03-31 09:05:36 +1300

    Fixed trinity tests to use network R and removed old R tests

--- apps/R-core/tests/gplots/plotmeans_nobars.R
+++
@@ -1,7 +0,0 @@
-## Test that plotmeans works properly when bars=F
-## Bug repoted by Alan Yong, Research Geophysicist,
-## US DEPARTMENT OF THE INTERIOR
-
-library(gplots)
-data(state)
-plotmeans(state.area ~ state.region, bars=FALSE)
